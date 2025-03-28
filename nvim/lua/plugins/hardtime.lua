b37a6fdcdf570fabb0cd872e4e5a8120b7e15937
Simon Ho <simonho.ubc@gmail.com>
2025-03-27 23:52:23 -0700

    feat(nvim): allow arrows keys

--- nvim/lua/plugins/hardtime.lua
+++ nvim/lua/plugins/hardtime.lua
@@ -4,5 +4,11 @@ return {
   dependencies = { "MunifTanjim/nui.nvim" },
   opts = {
     disable_mouse = false,
+    disabled_keys = {
+      ["<Up>"] = {},
+      ["<Down>"] = {},
+      ["<Left>"] = {},
+      ["<Right>"] = {},
+    },
   },
 }
