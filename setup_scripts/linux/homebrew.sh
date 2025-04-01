44491511521cc48f384a180d519daa1d4c7d216b
Simon Ho <simonho.ubc@gmail.com>
2025-03-31 23:12:07 -0700

    feat(wezterm): add github fonts

--- setup_scripts/linux/homebrew.sh
+++ setup_scripts/linux/homebrew.sh
@@ -2,6 +2,7 @@
 # https://brew.sh
 
 brew install bat
+brew install chafa
 brew install fd
 brew install ffmpeg
 brew install fzf
@@ -21,10 +22,11 @@ brew install zoxide
 
 # Fonts
 fonts_list=(
-  font-monofur-nerd-font
   font-fira-code-nerd-font
-  font-jetbrains-mono-nerd-font
+  font-monaspace-nerd-font
+  font-monofur-nerd-font
   font-departure-mono-nerd-font
+  font-jetbrains-mono-nerd-font
 )
 
 for font in "${fonts_list[@]}"; do
