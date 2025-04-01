09492541344b0beb71795faadf59a1156a0cf051
Simon Ho <simonho.ubc@gmail.com>
2025-03-31 23:12:31 -0700

    feat(nvim): add diagnostics to scrollbar

--- nvim/lua/plugins/scrollbar.lua
+++ nvim/lua/plugins/scrollbar.lua
@@ -8,7 +8,18 @@ return {
       excluded_filetypes = { "TelescopePrompt", "bigfile", "which-key" },
       handlers = {
         cursor = {
+          enable = true,
           symbols = { "●" },
+        },
+        diagnostic = {
+          enable = true,
+          signs = {
+            error = { LazyVim.config.icons.diagnostics.Error },
+            warn = { LazyVim.config.icons.diagnostics.Warn },
+            info = { LazyVim.config.icons.diagnostics.Info },
+            hint = { LazyVim.config.icons.diagnostics.Hint },
+          },
+          min_severity = vim.diagnostic.severity.WARN,
         },
       },
     },
