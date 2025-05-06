17b88cd7f2c5bbf38317582b869cbcef8adf7393
Simon Ho <simonho.ubc@gmail.com>
2025-05-05 22:09:11 -0700

    feat(setup): update setup apps

--- setup_scripts/linux/work.sh
+++ setup_scripts/linux/work.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Applications for work
 
-flatpak install flathub com.slack.Slack
+flatpak install flathub com.slack.Slack -y
 
+brew install go@1.22
 brew install python@3.11
-brew install awscli@1
+brew install awscli
 
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
