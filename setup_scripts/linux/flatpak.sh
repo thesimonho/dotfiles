05002abe68579ffb04c815e2ba6a4da4f3d3c50b
Simon Ho <simonho.ubc@gmail.com>
2025-05-05 22:09:11 -0700

    feat(setup): update setup apps

--- setup_scripts/linux/flatpak.sh
+++ setup_scripts/linux/flatpak.sh
@@ -4,13 +4,14 @@
 flatpak install flathub com.jeffser.Alpaca -y
 flatpak install com.jeffser.Alpaca.Plugins.Ollama -y
 flatpak install flathub eu.betterbird.Betterbird -y
-flatpak install flathub com.calibre_ebook.calibre
+flatpak install flathub com.calibre_ebook.calibre -y
 flatpak install flathub com.google.Chrome -y
-flatpak install flathub io.dbeaver.DBeaverCommunity
+flatpak install flathub io.dbeaver.DBeaverCommunity -y
 flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.ranfdev.DistroShelf -y
 flatpak install flathub com.dropbox.Client -y
-flatpak install flathub it.mijorus.gearlever -y
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub org.kde.kolourpaint -y
 flatpak install flathub org.libreoffice.LibreOffice -y
-flatpak install flathub io.github.berarma.Oversteer
+flatpak install flathub io.github.berarma.Oversteer -y
+flatpak install flathub io.podman_desktop.PodmanDesktop -y
--- setup_scripts/linux/homebrew.sh
+++ setup_scripts/linux/homebrew.sh
@@ -4,11 +4,12 @@
 brew install bat
 brew install carapace
 brew install chafa
+brew install distrobox
+# brew install docker
 brew install fd
 brew install ffmpeg
-brew install fzf
+# brew install fzf
 brew install gh
-brew install go
 brew install lazygit
 brew install luarocks
 brew install neovim
