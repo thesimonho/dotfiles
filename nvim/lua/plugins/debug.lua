91ef1e0434e0716262b48ace098a66997881fe6d
Simon Ho <simonho.ubc@gmail.com>
2025-05-10 05:56:56 -0700

    refactor(nvim): create lua language file

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,18 +1,5 @@
 return {
   {
-    "mfussenegger/nvim-dap",
-    keys = {
-      {
-        "<leader>dS",
-        function()
-          require("osv").launch({ port = 8086 })
-        end,
-        desc = "Start Nvim Lua Server",
-        ft = "lua",
-      },
-    },
-  },
-  {
     "rcarriga/nvim-dap-ui",
     opts = {
       icons = {
