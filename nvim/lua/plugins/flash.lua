539a5f023f51653b7fccb72c77557a9a0b84a923
Simon Ho <simonho.ubc@gmail.com>
2025-05-13 22:38:19 -0700

    feat(nvim): add eyeliner

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,60 +1,71 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  keys = {
-    {
-      "s",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump({ search = { forward = true } })
-      end,
-      desc = "Flash forward",
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({ search = { forward = true } })
+        end,
+        desc = "Flash forward",
+      },
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({ search = { forward = false } })
+        end,
+        desc = "Flash backward",
+      },
+      {
+        "<M-s>",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
     },
-    {
-      "S",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump({ search = { forward = false } })
-      end,
-      desc = "Flash backward",
-    },
-    {
-      "<M-s>",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").treesitter()
-      end,
-      desc = "Flash Treesitter",
+    opts = {
+      search = {
+        multi_window = false,
+        incremental = true,
+        forward = true,
+        wrap = false,
+      },
+      jump = {
+        nohlsearch = false,
+      },
+      label = {
+        uppercase = false,
+        before = false,
+        after = true,
+        style = "overlay",
+        rainbow = {
+          enabled = false,
+        },
+      },
+      modes = {
+        search = {
+          enabled = true,
+        },
+        char = {
+          enabled = false, -- operator pending mode doesnt work like normal vim & interferes with eyeliner
+        },
+      },
     },
   },
-  opts = {
-    search = {
-      multi_window = false,
-      incremental = true,
-      forward = true,
-      wrap = false,
-    },
-    jump = {
-      nohlsearch = false,
-    },
-    label = {
-      uppercase = false,
-      before = false,
-      after = true,
-      style = "overlay",
-      rainbow = {
-        enabled = false,
-      },
-    },
-    modes = {
-      search = {
-        enabled = true,
-      },
-      char = {
-        enabled = false, -- nice, but operator pending mode doesnt work like normal vim
-        jump_labels = true,
-        multi_line = false,
-      },
+  {
+    "jinh0/eyeliner.nvim",
+    event = "LazyFile",
+    opts = {
+      -- show highlights only after keypress
+      highlight_on_key = true,
+      -- dim all other characters if set to true (recommended!)
+      dim = true,
+      default_keymaps = true,
     },
   },
 }
