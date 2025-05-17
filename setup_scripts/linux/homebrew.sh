adcc6df439235f27918cd4108c550683abc6840e
Simon Ho <simonho.ubc@gmail.com>
2025-05-17 02:10:38 -0700

    feat(setup): add check for existing cli tools

--- setup_scripts/linux/homebrew.sh
+++ setup_scripts/linux/homebrew.sh
@@ -1,24 +1,59 @@
 #!/bin/bash
 # https://brew.sh
 
-brew install bat
-brew install carapace
-brew install chafa
-brew install distrobox
-brew install fd
-brew install ffmpeg
-brew install gh
-brew install lazygit
-brew install luarocks
-brew install neovim
-brew install node
-brew install nushell
-brew install pandoc
-brew install ripgrep
-brew install sqlite
-brew install starship
-brew install yazi sevenzip jq poppler imagemagick
-brew install zoxide
+# Map of brew package name to CLI entrypoint
+declare -A packages=(
+  [bat]="bat"
+  [carapace]="carapace"
+  [chafa]="chafa"
+  [distrobox]="distrobox"
+  [direnv]="direnv"
+  [fd]="fd"
+  [ffmpeg]="ffmpeg"
+  [fzf]="fzf"
+  [gh]="gh"
+  [lazygit]="lazygit"
+  [luarocks]="luarocks"
+  [neovim]="nvim"
+  [node]="node"
+  [nushell]="nu"
+  [pandoc]="pandoc"
+  [ripgrep]="rg"
+  [sqlite]="sqlite3"
+  [starship]="starship"
+  [tealdeer]="tldr"
+  [yazi]="yazi"
+  [sevenzip]="7zz"
+  [jq]="jq"
+  [poppler]="pdftotext"
+  [imagemagick]="magick"
+  ['uutils-coreutils']="uutils"
+  [zoxide]="zoxide z"
+)
+
+to_install=()
+
+for pkg in "${!packages[@]}"; do
+  entrypoints="${packages[$pkg]}"
+  found=0
+  for entry in $entrypoints; do
+    if command -v "$entry" >/dev/null 2>&1; then
+      echo "$pkg: found ($entry), skipping."
+      found=1
+      break
+    fi
+  done
+  if [ $found -eq 0 ]; then
+    to_install+=("$pkg")
+  fi
+done
+
+if [ "${#to_install[@]}" -gt 0 ]; then
+  echo "Installing: ${to_install[*]}"
+  brew install "${to_install[@]}"
+else
+  echo "All CLI tools already installed."
+fi
 
 # Fonts
 fonts_list=(
