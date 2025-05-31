0cf5194139697b0f496df50e050444086807e647
Simon Ho <simonho.ubc@gmail.com>
2025-05-31 01:40:28 -0700

    feat(nvim): add nvim surround

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ai.supermaven" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
