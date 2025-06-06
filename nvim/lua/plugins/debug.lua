f839ef288f67955874a7420e95b99f3a8428e409
Simon Ho <simonho.ubc@gmail.com>
2025-06-06 02:52:04 -0700

    feat(nvim): replace dap ui with dap view

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,6 +1,54 @@
 return {
   {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "igorlfs/nvim-dap-view",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+  },
+  {
+    "igorlfs/nvim-dap-view",
+    keys = {
+      { "<leader>dv", "<cmd>lua require('dap-view').toggle()<cr>", desc = "Toggle DAP View" },
+    },
+    init = function()
+      local dap, dv = require("dap"), require("dap-view")
+      dap.listeners.before.attach["dap-view-config"] = function()
+        dv.open()
+      end
+      dap.listeners.before.launch["dap-view-config"] = function()
+        dv.open()
+      end
+      dap.listeners.before.event_terminated["dap-view-config"] = function()
+        dv.close()
+      end
+      dap.listeners.before.event_exited["dap-view-config"] = function()
+        dv.close()
+      end
+
+      vim.api.nvim_create_autocmd({ "FileType" }, {
+        pattern = { "dap-view", "dap-view-term", "dap-repl" },
+        callback = function(evt)
+          vim.keymap.set("n", "q", "<C-w>q", { buffer = evt.buf })
+        end,
+      })
+    end,
+    opts = {
+      winbar = {
+        controls = {
+          enabled = true,
+        },
+      },
+      windows = {
+        terminal = {
+          hide = { "go" },
+        },
+      },
+    },
+  },
+  {
     "rcarriga/nvim-dap-ui",
+    enabled = false,
     opts = {
       icons = {
         collapsed = "",
