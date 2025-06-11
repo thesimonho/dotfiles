d567a85e4d3dd7d5744586aebd48bf6c3497032a
Simon Ho <simonho.ubc@gmail.com>
2025-06-11 03:00:43 -0700

    feat(nvim): add theme reload function

--- nvim/lua/plugins/colourschemes/kanagawa-paper.lua
+++ nvim/lua/plugins/colourschemes/kanagawa-paper.lua
@@ -1,19 +1,36 @@
+local opts = {
+  cache = false,
+  dim_inactive = true,
+  diag_background = true,
+  auto_plugins = true,
+  integrations = {
+    wezterm = {
+      enabled = true,
+    },
+  },
+}
+
 return {
   {
     "thesimonho/kanagawa-paper.nvim",
     lazy = false,
     dev = true,
     priority = 1000,
-    opts = {
-      cache = false,
-      dim_inactive = true,
-      diag_background = true,
-      auto_plugins = true,
-      integrations = {
-        wezterm = {
-          enabled = true,
-        },
+    keys = {
+      {
+        "<leader>zC",
+        function()
+          for k in pairs(package.loaded) do
+            if k:match("^kanagawa%-paper") then
+              package.loaded[k] = nil
+            end
+          end
+          require("kanagawa-paper").setup(opts)
+          vim.cmd.colorscheme("kanagawa-paper")
+        end,
+        desc = "Colorscheme Reload",
       },
     },
+    opts = opts,
   },
 }
