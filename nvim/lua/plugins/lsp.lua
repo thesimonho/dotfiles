c1535d9522efb55a89c905cf734165acabbc1a97
Simon Ho <simonho.ubc@gmail.com>
2025-06-17 21:38:32 -0700

    feat(nvim): remove garbage day

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -56,14 +56,4 @@ return {
       },
     },
   },
-  {
-    "zeioth/garbage-day.nvim",
-    dependencies = "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-    opts = {
-      excluded_lsp_clients = { "copilot" },
-      grace_period = 60 * 15, --seconds
-      wakeup_delay = 2000, --milliseconds
-    },
-  },
 }
