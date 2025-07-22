98aae58bcac0f12c901db896015d05d0a7375419
Simon Ho <simonho.ubc@gmail.com>
2025-07-22 01:04:19 -0700

    feat(nvim): add vectorcode

--- nvim/lua/plugins/AI.lua
+++ nvim/lua/plugins/AI.lua
@@ -1,5 +1,5 @@
 local style = require("utils.style")
-vim.g.ai_cmp = true -- show AI suggestions in cmp
+vim.g.ai_cmp = false -- show AI suggestions in cmp
 
 local M = {
   {
@@ -53,6 +53,7 @@ local M = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "ravitemer/codecompanion-history.nvim",
+      "Davidyz/VectorCode",
     },
     keys = {
       { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", mode = "n", desc = "Chat" },
@@ -123,6 +124,9 @@ local M = {
             opts = {
               auto_submit_errors = true,
               auto_submit_success = true,
+              default_tools = {
+                "vectorcode_toolbox",
+              },
             },
           },
           opts = {
@@ -177,6 +181,52 @@ local M = {
             delete_on_clearing_chat = false,
           },
         },
+        vectorcode = {
+          enabled = true,
+          opts = {
+            tool_group = {
+              enabled = true,
+              collapse = true,
+              extras = { "file_search" },
+            },
+            tool_opts = {
+              ["*"] = {},
+              ls = {},
+              vectorise = {},
+              query = {
+                max_num = { chunk = -1, document = -1 },
+                default_num = { chunk = 50, document = 10 },
+                include_stderr = false,
+                no_duplicate = true,
+                chunk_mode = false,
+                summarise = {
+                  enabled = true,
+                  query_augmented = true,
+                },
+              },
+              files_ls = {},
+              files_rm = {},
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "Davidyz/VectorCode",
+    build = "CC=gcc CXX=g++ uv tool upgrade vectorcode",
+    opts = {
+      async_opts = {
+        exclude_this = true,
+        n_query = 1,
+        notify = true,
+        run_on_register = true,
+      },
+      async_backend = "default", -- or "lsp"
+      exclude_this = true,
+      notify = true,
+      on_setup = {
+        update = true,
       },
     },
   },
