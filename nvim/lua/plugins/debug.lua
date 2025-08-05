c48a34f2372a02fd51df72897798dc5955ee8782
Simon Ho <simonho.ubc@gmail.com>
2025-08-04 21:01:30 -0700

    feat(nvim): remove debugprint

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -104,41 +104,4 @@ return {
       virt_text_pos = "eol",
     },
   },
-  {
-    "andrewferrier/debugprint.nvim",
-    lazy = false, -- Required to make line highlighting work before debugprint is first used
-    version = "*", -- Remove if you DON'T want to use the stable version
-    init = function()
-      local wk = require("which-key")
-      wk.add({
-        mode = { "n", "v" },
-        { "<leader>dd", group = "debug print" },
-      })
-    end,
-    opts = {
-      display_counter = false,
-      keymaps = {
-        normal = {
-          plain_below = "<leader>ddp",
-          plain_above = "",
-          variable_below = "<leader>ddv",
-          variable_above = "",
-          variable_below_alwaysprompt = "",
-          variable_above_alwaysprompt = "",
-          surround_plain = "",
-          surround_variable = "<leader>dds",
-          surround_variable_alwaysprompt = "",
-          textobj_below = "<leader>ddo",
-          textobj_above = "",
-          textobj_surround = "",
-          toggle_comment_debug_prints = "<leader>ddC",
-          delete_debug_prints = "<leader>ddD",
-        },
-        visual = {
-          variable_below = "<leader>ddv",
-          variable_above = "",
-        },
-      },
-    },
-  },
 }
