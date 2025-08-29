615237e0654ea1116c0250344e25bcd066b74459
Simon Ho <simonho.ubc@gmail.com>
2025-08-29 02:03:43 -0700

    feat(nvim): replace ts context with navic

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -56,41 +56,4 @@ return {
       },
     },
   },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "LazyFile",
-    init = function()
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "*.md",
-        callback = function()
-          require("treesitter-context").disable()
-        end,
-      })
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "*",
-        callback = function()
-          if not vim.fn.expand("%:e"):match("^md$") then
-            require("treesitter-context").enable()
-          end
-        end,
-      })
-    end,
-    keys = {
-      {
-        "gC",
-        function()
-          require("treesitter-context").go_to_context(vim.v.count1)
-        end,
-        desc = "Go to Treesitter Context",
-      },
-    },
-    opts = {
-      multiwindow = false, -- Enable multiwindow support.
-      max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
-      line_numbers = true,
-      multiline_threshold = 3, -- Maximum number of lines to show for a single context
-      trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-      mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
-    },
-  },
 }
