970df7a22c0b07a9e76b09b81e0895f748e4e6cf
Simon Ho <simonho.ubc@gmail.com>
2025-09-16 00:22:25 -0700

    feat(nvim): add debug binds

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,15 +1,38 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "igorlfs/nvim-dap-view",
-      "theHamsta/nvim-dap-virtual-text",
-    },
-  },
+  { "rcarriga/nvim-dap-ui", enabled = false },
   {
     "igorlfs/nvim-dap-view",
     keys = {
       { "<leader>dv", "<cmd>lua require('dap-view').toggle()<cr>", desc = "Toggle DAP View" },
+      { "<leader>dW", "<cmd>lua require('dap-view').add_expr()<cr>", desc = "Add Watch" },
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+      {
+        "<F10>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<F11>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<F12>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
     },
     init = function()
       local dap, dv = require("dap"), require("dap-view")
@@ -34,9 +57,20 @@ return {
       })
     end,
     opts = {
+      auto_toggle = true,
       winbar = {
         controls = {
           enabled = true,
+          buttons = {
+            "play",
+            "step_back",
+            "step_over",
+            "step_into",
+            "step_out",
+            "run_last",
+            "terminate",
+            "disconnect",
+          },
         },
       },
       windows = {
@@ -44,54 +78,15 @@ return {
           hide = { "go" },
         },
       },
-    },
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    enabled = false,
-    opts = {
       icons = {
-        collapsed = "",
-        current_frame = "",
-        expanded = "",
+        play = " F5",
+        step_back = " F9",
+        step_over = " F10",
+        step_into = " F11",
+        step_out = " F12",
       },
-      layouts = {
-        {
-          elements = {
-            {
-              id = "scopes",
-              size = 0.25,
-            },
-            {
-              id = "breakpoints",
-              size = 0.25,
-            },
-            {
-              id = "stacks",
-              size = 0.25,
-            },
-            {
-              id = "watches",
-              size = 0.25,
-            },
-          },
-          position = "left",
-          size = 50,
-        },
-        {
-          elements = {
-            {
-              id = "repl",
-              size = 0.5,
-            },
-            {
-              id = "console",
-              size = 0.5,
-            },
-          },
-          position = "bottom",
-          size = 10,
-        },
+      help = {
+        border = "rounded",
       },
     },
   },
@@ -100,7 +95,7 @@ return {
     opts = {
       all_references = true,
       highlight_changed_variables = true,
-      highlight_new_as_changed = true,
+      highlight_new_as_changed = false,
       virt_text_pos = "eol",
     },
   },
