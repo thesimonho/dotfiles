9a27aebd859b0c29a8fda9b77d6ff29aa54804e6
Simon Ho <simonho.ubc@gmail.com>
2025-10-07 01:54:53 -0700

    feat(nvim): update sidekick config

--- nvim/lua/plugins/AI.lua
+++ nvim/lua/plugins/AI.lua
@@ -34,25 +34,8 @@ local M = {
   {
     "folke/sidekick.nvim",
     keys = {
-      {
-        "<tab>",
-        function()
-          -- if there is a next edit, jump to it, otherwise apply it if any
-          if not require("sidekick").nes_jump_or_apply() then
-            return "<Tab>" -- fallback to normal tab
-          end
-        end,
-        expr = true,
-        desc = "Goto/Apply Next Edit Suggestion",
-      },
-      {
-        "<c-.>",
-        function()
-          require("sidekick.cli").focus()
-        end,
-        mode = { "n", "x", "i", "t" },
-        desc = "Sidekick Switch Focus",
-      },
+      { "<leader>aa", vim.NIL },
+      { "<leader>ac", vim.NIL },
     },
     opts = {
       cli = {
