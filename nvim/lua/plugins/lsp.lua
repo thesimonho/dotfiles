c64a64f9c2c00901773507bff0195d17836e71e7
Simon Ho <simonho.ubc@gmail.com>
2025-10-07 01:54:44 -0700

    feat(nvim): add sql lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
+        "postgrestools", -- supabase
         "graphql-language-service-cli",
         "vim-language-server",
         "yaml-language-server",
