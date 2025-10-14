5f404a9a6b98f89b552b9b3a5f4edfcb4855ff2c
Simon Ho <simonho.ubc@gmail.com>
2025-10-14 01:36:46 -0700

    feat(nvim): add smart splits

--- nvim/lua/plugins/buffers.lua
+++ nvim/lua/plugins/buffers.lua
@@ -98,4 +98,73 @@ return {
       },
     },
   },
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    keys = {
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+        desc = "Move to left split",
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+        desc = "Move to down split",
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+        desc = "Move to up split",
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+        desc = "Move to right split",
+      },
+      {
+        "<C-A-h>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+        desc = "Resize split left",
+      },
+      {
+        "<C-A-j>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+        desc = "Resize split down",
+      },
+      {
+        "<C-A-k>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+        desc = "Resize split up",
+      },
+      {
+        "<C-A-l>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+        desc = "Resize split right",
+      },
+    },
+    opts = {
+      at_edge = "split",
+      cursor_follows_swapped_bufs = true,
+      disable_multiplexer_nav_when_zoomed = false,
+      zellij_move_focus_or_tab = false,
+      log_level = "warn",
+    },
+  },
 }
