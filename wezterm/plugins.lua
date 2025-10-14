3dfa5973cdd16ebc050ec630d439c49f33b5bf49
Simon Ho <simonho.ubc@gmail.com>
2025-10-13 21:02:33 -0700

    feat(wezterm): ability to toggle plugins

--- wezterm/plugins.lua
+++ wezterm/plugins.lua
@@ -1,157 +1,250 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+local keybinds = require("keybinds")
+local containers = require("containers")
 local theme = require("theme_switcher")
 
-local function find_binary(name)
-	local handle = io.popen("command -v " .. name .. " 2>/dev/null")
-	if not handle then
-		return nil
-	end
-
-	local result = handle:read("*a")
-	handle:close()
-
-	result = result:gsub("%s+", "")
-	if result == "" then
-		return nil
-	end
-	return result
-end
+local enabled = {
+	tabline = true,
+	workspace_switcher = true,
+	dev_containers = false,
+}
 
 local M = {}
 
--- workspace_switcher
-M.workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
-M.workspace_switcher.zoxide_path = find_binary("zoxide") or "/usr/bin/zoxide"
+-- dev containers
+if enabled.dev_containers then
+	local function handle_selection(window, pane, _, label)
+		if not label then
+			-- no selection
+			return
+		end
 
-M.workspace_switcher.workspace_formatter = function(label)
-	return wezterm.format({
-		{ Attribute = { Intensity = "Bold" } },
-		{ Foreground = { Color = "#8ea4a2" } },
-		{ Text = "󱂬 : " .. label },
-	})
+		if label == "󰌙 detach domains" then
+			local domains = wezterm.mux.all_domains()
+			for _, domain in ipairs(domains) do
+				if domain:state() == "Attached" and domain:name() ~= "local" and domain:is_spawnable() then
+					domain:detach()
+				end
+			end
+			return
+		elseif label == "󰑓 reload domains" then
+			window:perform_action(act.ReloadConfiguration, pane)
+			return
+		end
+
+		local kind, name = label:match("^(.-): (.-)%s*%*?$")
+		if kind == "devpod" then
+			window:perform_action(
+				act.SwitchToWorkspace({
+					name = name,
+					spawn = {
+						domain = { DomainName = name },
+					},
+				}),
+				pane
+			)
+		elseif kind == "distrobox" then
+			window:perform_action(
+				act.SpawnCommandInNewTab({
+					args = { "distrobox", "enter", "--root", name },
+				}),
+				pane
+			)
+		else
+			window:perform_action(
+				act.SwitchToWorkspace({
+					name = label,
+					spawn = {
+						domain = { DomainName = label },
+					},
+				}),
+				pane
+			)
+		end
+	end
+
+	-- Creates the input selector action
+	local function show_domain_selector()
+		-- create choices every time so we can update the * indicator
+		local choices = containers.create_container_choices()
+
+		return act.InputSelector({
+			action = wezterm.action_callback(handle_selection),
+			choices = choices,
+			alphabet = "1234qwer",
+			description = "Attach domain:",
+		})
+	end
+
+	keybinds.basic_binds[#keybinds.basic_binds + 1] = {
+		key = "p",
+		mods = "SUPER",
+		action = wezterm.action_callback(function(window, pane)
+			window:perform_action(show_domain_selector(), pane)
+		end),
+	}
 end
 
-wezterm.on("smart_workspace_switcher.workspace_switcher.chosen", function(window, workspace)
-	local gui_win = window:gui_window()
-	local base_path = string.gsub(workspace, "(.*[/\\])(.*)", "%2")
-	gui_win:set_right_status(wezterm.format({
-		{ Attribute = { Intensity = "Bold" } },
-		{ Foreground = { Color = "#8ea4a2" } },
-		{ Text = "󱂬 : " .. base_path .. " " },
-	}))
-end)
+-- workspace_switcher
+if enabled.workspace_switcher then
+	local function find_binary(name)
+		local handle = io.popen("command -v " .. name .. " 2>/dev/null")
+		if not handle then
+			return nil
+		end
 
-wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, workspace)
-	local gui_win = window:gui_window()
-	local base_path = string.gsub(workspace, "(.*[/\\])(.*)", "%2")
-	gui_win:set_right_status(wezterm.format({
-		{ Attribute = { Intensity = "Bold" } },
-		{ Foreground = { Color = "#8ea4a2" } },
-		{ Text = "󱂬 : " .. base_path .. " " },
-	}))
-end)
+		local result = handle:read("*a")
+		handle:close()
+
+		result = result:gsub("%s+", "")
+		if result == "" then
+			return nil
+		end
+		return result
+	end
+
+	M.workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+	M.workspace_switcher.zoxide_path = find_binary("zoxide") or "/usr/bin/zoxide"
+
+	M.workspace_switcher.workspace_formatter = function(label)
+		return wezterm.format({
+			{ Attribute = { Intensity = "Bold" } },
+			{ Foreground = { Color = "#8ea4a2" } },
+			{ Text = "󱂬 : " .. label },
+		})
+	end
+
+	wezterm.on("smart_workspace_switcher.workspace_switcher.chosen", function(window, workspace)
+		local gui_win = window:gui_window()
+		local base_path = string.gsub(workspace, "(.*[/\\])(.*)", "%2")
+		gui_win:set_right_status(wezterm.format({
+			{ Attribute = { Intensity = "Bold" } },
+			{ Foreground = { Color = "#8ea4a2" } },
+			{ Text = "󱂬 : " .. base_path .. " " },
+		}))
+	end)
+
+	wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, workspace)
+		local gui_win = window:gui_window()
+		local base_path = string.gsub(workspace, "(.*[/\\])(.*)", "%2")
+		gui_win:set_right_status(wezterm.format({
+			{ Attribute = { Intensity = "Bold" } },
+			{ Foreground = { Color = "#8ea4a2" } },
+			{ Text = "󱂬 : " .. base_path .. " " },
+		}))
+	end)
+
+	keybinds.basic_binds[#keybinds.basic_binds + 1] = {
+		key = "p",
+		mods = "SUPER",
+		action = M.workspace_switcher.switch_workspace(),
+	}
+end
 
 -- tabline
-M.tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
-M.tabline.setup({
-	options = {
-		icons_enabled = true,
-		theme = require("colors.wezterm_tabline." .. theme.color_scheme),
-		section_separators = {
-			left = wezterm.nerdfonts.ple_right_half_circle_thick,
-			right = wezterm.nerdfonts.ple_left_half_circle_thick,
+if enabled.tabline then
+	M.tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+	M.tabline.setup({
+		options = {
+			icons_enabled = true,
+			theme = require("colors.wezterm_tabline." .. theme.color_scheme),
+			section_separators = {
+				left = wezterm.nerdfonts.ple_right_half_circle_thick,
+				right = wezterm.nerdfonts.ple_left_half_circle_thick,
+			},
+			component_separators = {
+				left = wezterm.nerdfonts.ple_right_half_circle_thin,
+				right = wezterm.nerdfonts.ple_left_half_circle_thin,
+			},
+			tab_separators = {
+				left = " ",
+				right = "",
+			},
 		},
-		component_separators = {
-			left = wezterm.nerdfonts.ple_right_half_circle_thin,
-			right = wezterm.nerdfonts.ple_left_half_circle_thin,
-		},
-		tab_separators = {
-			left = " ",
-			right = "",
-		},
-	},
-	sections = {
-		tabline_a = {
-			{
-				"mode",
-				icon = "🐼",
-				fmt = function(text)
-					return string.lower(text)
+		sections = {
+			tabline_a = {
+				{
+					"mode",
+					icon = "🐼",
+					fmt = function(text)
+						return string.lower(text)
+					end,
+				},
+			},
+			tabline_b = {
+				{ "workspace", padding = { left = 1, right = 0 } },
+				-- {
+				-- 	"domain",
+				-- 	padding = { left = 1, right = 0 },
+				-- 	cond = function()
+				-- 		if #wezterm.mux.all_domains() > 2 then
+				-- 			return true
+				-- 		end
+				-- 		return false
+				-- 	end,
+				-- },
+			},
+			tabline_c = { " " },
+			tabline_x = {
+				function(window)
+					local metadata = window:active_pane():get_metadata()
+					if not metadata then
+						return ""
+					end
+
+					local latency = metadata.since_last_response_ms
+					if not latency then
+						return ""
+					end
+
+					local color
+					local icon
+					local red = "\27[31m"
+					local yellow = "\27[33m"
+					local green = "\27[32m"
+					if metadata.is_tardy then
+						if latency > 10000 then
+							color = red
+							icon = "󰢼"
+							latency = ">999"
+						else
+							color = yellow
+							icon = "󰢽"
+						end
+					else
+						color = green
+						icon = "󰢾"
+						latency = "<1"
+					end
+					return string.format(color .. icon .. " %sms ", latency)
 				end,
 			},
-		},
-		tabline_b = {
-			{ "workspace", padding = { left = 1, right = 0 } },
-			-- {
-			-- 	"domain",
-			-- 	padding = { left = 1, right = 0 },
-			-- 	cond = function()
-			-- 		if #wezterm.mux.all_domains() > 2 then
-			-- 			return true
-			-- 		end
-			-- 		return false
-			-- 	end,
-			-- },
-		},
-		tabline_c = { " " },
-		tabline_x = {
-			function(window)
-				local metadata = window:active_pane():get_metadata()
-				if not metadata then
-					return ""
-				end
-
-				local latency = metadata.since_last_response_ms
-				if not latency then
-					return ""
-				end
-
-				local color
-				local icon
-				local red = "\27[31m"
-				local yellow = "\27[33m"
-				local green = "\27[32m"
-				if metadata.is_tardy then
-					if latency > 10000 then
-						color = red
-						icon = "󰢼"
-						latency = ">999"
-					else
-						color = yellow
-						icon = "󰢽"
-					end
-				else
-					color = green
-					icon = "󰢾"
-					latency = "<1"
-				end
-				return string.format(color .. icon .. " %sms ", latency)
-			end,
-		},
-		tabline_y = {
-			{
-				"datetime",
-				style = "%A %b %d",
-				icon = "",
-				hour_to_icon = false,
+			tabline_y = {
+				{
+					"datetime",
+					style = "%A %b %d",
+					icon = "",
+					hour_to_icon = false,
+				},
+			},
+			tabline_z = { "hostname" },
+			tab_active = {
+				{ "process", icons_only = true, padding = { left = 2, right = 0 } },
+				{ "parent", max_length = 10, padding = 0 },
+				"/",
+				{ "cwd", max_length = 15, padding = { left = 0, right = 2 } },
+				{ "zoomed", padding = 0 },
+			},
+			tab_inactive = {
+				{ "process", icons_only = true, padding = { left = 2, right = 0 } },
+				{ "parent", max_length = 10, padding = 0 },
+				"/",
+				{ "cwd", max_length = 15, padding = { left = 0, right = 2 } },
 			},
 		},
-		tabline_z = { "hostname" },
-		tab_active = {
-			{ "process", icons_only = true, padding = { left = 2, right = 0 } },
-			{ "parent", max_length = 10, padding = 0 },
-			"/",
-			{ "cwd", max_length = 15, padding = { left = 0, right = 2 } },
-			{ "zoomed", padding = 0 },
-		},
-		tab_inactive = {
-			{ "process", icons_only = true, padding = { left = 2, right = 0 } },
-			{ "parent", max_length = 10, padding = 0 },
-			"/",
-			{ "cwd", max_length = 15, padding = { left = 0, right = 2 } },
-		},
-	},
-	extensions = { "smart_workspace_switcher" },
-})
+		extensions = { "smart_workspace_switcher" },
+	})
+end
 
 return M
