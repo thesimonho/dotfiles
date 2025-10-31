45f70b92537ca90e6ef86d0145a3acbe4db85026
Simon Ho <simonho.ubc@gmail.com>
2025-10-31 12:38:02 -0700

    feat(nvim): remove hydra

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,44 +1,7 @@
 local constants = require("config.constants")
 
-local hydra = { active = false }
--- prevent recursive calls to show which key menu if hydra is already open
-local function hydra_show(keys)
-  if hydra.active then
-    -- already in which-key loop: pass the key through to which-key
-    -- use non-remap so it doesn't trigger this mapping again
-    vim.api.nvim_feedkeys(keys, "n", false)
-    return
-  end
-  hydra.active = true
-  local ok, err = pcall(function()
-    require("which-key").show({ keys = keys, loop = true })
-  end)
-  hydra.active = false
-  if not ok and err then
-    vim.notify(err, vim.log.levels.ERROR)
-  end
-end
-
 return {
   "folke/which-key.nvim",
-  keys = {
-    {
-      "[",
-      function()
-        hydra_show("[")
-      end,
-      mode = "n",
-      desc = "hydra: [",
-    },
-    {
-      "]",
-      function()
-        hydra_show("]")
-      end,
-      mode = "n",
-      desc = "hydra: ]",
-    },
-  },
   opts = {
     show_help = false,
     show_keys = false,
