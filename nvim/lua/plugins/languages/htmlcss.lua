032b83b900112338ee413f1f6a8d4a80e63a2cee
Simon Ho <simonho.ubc@gmail.com>
2025-11-18 11:16:47 -0800

    feat(nvim): add emmet bind

--- nvim/lua/plugins/languages/htmlcss.lua
+++ nvim/lua/plugins/languages/htmlcss.lua
@@ -1,13 +1,31 @@
 return {
   {
     "mason-org/mason.nvim",
-    keys = {
-      { "<leader>cm", vim.NIL },
-    },
     opts = {
       ensure_installed = {
         -- lsp
         "emmet-language-server",
+      },
+    },
+  },
+  {
+    "olrtg/nvim-emmet",
+    ft = { "html", "css", "sass", "scss", "vue", "javascriptreact", "typescriptreact" },
+    init = function()
+      local wk = require("which-key")
+      wk.add({
+        mode = { "n", "v" },
+        { "<localleader>e", group = "Emmet" },
+      })
+    end,
+    keys = {
+      {
+        "<localleader>ew",
+        function()
+          require("nvim-emmet").wrap_with_abbreviation()
+        end,
+        mode = { "n", "v" },
+        desc = "Wrap with Abbreviation",
       },
     },
   },
