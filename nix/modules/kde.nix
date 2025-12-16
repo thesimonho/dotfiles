924c2fcfdc40961c8f5e9d1e1ea7ae7578f3d0cd
Simon Ho <simonho.ubc@gmail.com>
2025-12-16 14:40:40 -0800

    feat(nix): plasma options

--- nix/modules/kde.nix
+++ nix/modules/kde.nix
@@ -1,15 +1,24 @@
 { pkgs, ... }: {
-  programs.plasma = { enable = true; };
-
-  programs.konsole = {
-    enable = true;
-    defaultProfile = "zsh";
-    profiles = {
-      zsh = {
-        command = "${pkgs.zsh}/bin/zsh";
-        font.name = "Fira Code";
-        font.size = 12;
-        colorScheme = "Breeze";
+  programs = {
+    plasma = {
+      enable = true;
+      input.keyboard = {
+        numlockOnStartup = "on";
+        repeatDelay = 100;
+        repeatRate = 60;
+      };
+      kwin.effects = { wobblyWindows.enable = true; };
+    };
+    konsole = {
+      enable = true;
+      defaultProfile = "zsh";
+      profiles = {
+        zsh = {
+          command = "${pkgs.zsh}/bin/zsh";
+          font.name = "Fira Code";
+          font.size = 12;
+          colorScheme = "Breeze";
+        };
       };
     };
   };
