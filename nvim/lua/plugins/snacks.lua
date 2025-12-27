26b58f0dbe414deffbbe536a828335436179ae9c
Simon Ho <simonho.ubc@gmail.com>
2025-12-27 01:00:26 -0800

    chore(nvim): simplify snacks styles

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -130,7 +130,7 @@ return {
       },
       notifier = {
         margin = { top = 1, right = 1, bottom = 1 },
-        style = "compact",
+        style = "fancy",
       },
       bigfile = { enabled = true },
       quickfile = { enabled = true },
@@ -306,37 +306,16 @@ return {
         },
       },
       styles = {
+        notification = {
+          wo = {
+            winblend = 10,
+          },
+        },
         above_cursor = {
-          backdrop = false,
-          position = "float",
-          border = "rounded",
           title_pos = "left",
-          height = 1,
-          noautocmd = true,
           relative = "cursor",
           row = -3,
           col = 0,
-          wo = {
-            cursorline = false,
-          },
-          bo = {
-            filetype = "snacks_input",
-            buftype = "prompt",
-          },
-          --- buffer local variables
-          b = {
-            completion = false, -- enable/disable blink completions in input
-          },
-          keys = {
-            n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
-            i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
-            i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = "i", expr = true },
-            i_tab = { "<tab>", { "cmp_select_next", "cmp", "fallback" }, mode = "i", expr = true },
-            i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
-            i_up = { "<up>", { "hist_up" }, mode = { "i", "n" } },
-            i_down = { "<down>", { "hist_down" }, mode = { "i", "n" } },
-            q = "cancel",
-          },
         },
       },
     },
