7ed918a5dcc2685252b474160f112d053153f16d
Simon Ho <simonho.ubc@gmail.com>
2025-12-27 19:59:39 -0800

    feat(nvim): use ccc for color highlighting
    it supports oklch colors

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -4,14 +4,23 @@ return {
     event = "LazyFile",
     opts = function()
       local ccc = require("ccc")
+      local mapping = ccc.mapping
       return {
+        default_color = "#636363",
         auto_close = true,
         preserve = true,
-        alpha_show = false,
+        alpha_show = "auto",
         save_on_quit = true,
+        lsp = true,
         highlighter = {
-          auto_enable = false, --- use just for color picker, no highlighting
+          auto_enable = true,
+          lsp = true,
+          update_insert = true,
+          excludes = { "lazy" },
         },
+        highlight_mode = "virtual",
+        virtual_symbol = "󰝤 ",
+        virtual_pos = "inline-left",
         recognize = {
           input = false,
           output = true,
@@ -24,58 +33,30 @@ return {
         outputs = {
           ccc.output.css_oklch,
           ccc.output.hex,
-          ccc.output.css_rgb,
           ccc.output.css_hsl,
+          ccc.output.css_rgb,
+        },
+        output_line = function(before_color, after_color)
+          local b_hex = before_color:hex()
+          local a_str = after_color:str()
+          local line = b_hex .. " -> " .. a_str
+          -- Range for highlight
+          local b_start_col = 0
+          local b_end_col = #b_hex
+          local a_start_col = b_end_col + 4
+          local a_end_col = a_start_col + #a_str
+          return line, b_start_col, b_end_col, a_start_col, a_end_col
+        end,
+        mappings = {
+          ["l"] = mapping.increase1,
+          ["L"] = mapping.increase10,
+          ["h"] = mapping.decrease1,
+          ["H"] = mapping.decrease10,
+          ["`"] = mapping.set0,
+          ["5"] = mapping.set50,
+          ["0"] = mapping.set100,
         },
       }
     end,
-  },
-  {
-    "brenoprata10/nvim-highlight-colors",
-    event = "LazyFile",
-    opts = {
-      --- 'background'|'foreground'|'virtual'
-      render = "virtual",
-
-      ---Set virtual symbol (requires render to be set to 'virtual')
-      virtual_symbol = "■",
-
-      ---Set virtual symbol suffix (defaults to '')
-      virtual_symbol_prefix = "",
-
-      ---Set virtual symbol suffix (defaults to ' ')
-      virtual_symbol_suffix = " ",
-
-      ---Set virtual symbol position()
-      ---inline mimics VS Code style
-      ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
-      ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
-      virtual_symbol_position = "inline",
-
-      ---Highlight hex colors, e.g. '#FFFFFF'
-      enable_hex = true,
-
-      ---Highlight short hex colors e.g. '#fff'
-      enable_short_hex = true,
-
-      ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
-      enable_rgb = true,
-
-      ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
-      enable_hsl = true,
-
-      ---Highlight CSS variables, e.g. 'var(--testing-color)'
-      enable_var_usage = true,
-
-      ---Highlight named colors, e.g. 'green'
-      enable_named_colors = true,
-
-      ---Highlight tailwind colors, e.g. 'bg-blue-500'
-      enable_tailwind = true,
-
-      -- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
-      exclude_filetypes = { "lazy", "ccc-ui" },
-      exclude_buftypes = { "text" },
-    },
   },
 }
