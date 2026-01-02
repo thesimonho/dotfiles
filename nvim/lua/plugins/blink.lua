cd90b948dc34d387e45ec499ac122a6bb009bccb
Simon Ho <simonho.ubc@gmail.com>
2026-01-01 16:04:52 -0800

    fix(nvim): C-f keymap typo

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -22,7 +22,7 @@ local M = {
         ["<Down>"] = { "select_next", "fallback" },
 
         ["<C-b>"] = { "scroll_documentation_up", "scroll_signature_up", "fallback" },
-        ["<C-f"] = { "scroll_documentation_down", "scroll_signature_down", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "scroll_signature_down", "fallback" },
 
         ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
       },
