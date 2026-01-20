6df841eb90480b4e8236f4056d1b1f3eb27583e4
Simon Ho <simonho.ubc@gmail.com>
2026-01-19 17:40:20 -0800

    feat(setup): install docker on arch

--- nix/post-setup.sh
+++ nix/post-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S --needed dolphin-plugins kio-gdrive extra/kde-graphics-meta extra/kde-system-meta
+sudo pacman -S --needed dolphin-plugins kio-gdrive extra/kde-graphics-meta extra/kde-system-meta docker
