ccfa6ec228ba4c6be50b221c048e57e7cad023b7
Simon Ho <simonho.ubc@gmail.com>
2026-01-19 17:53:54 -0800

    feat(setup): install podman

--- nix/post-setup.sh
+++ nix/post-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S --needed dolphin-plugins kio-gdrive extra/kde-graphics-meta extra/kde-system-meta docker
+sudo pacman -S --needed dolphin-plugins kio-gdrive extra/kde-graphics-meta extra/kde-system-meta docker podman podman-desktop podman-docker
