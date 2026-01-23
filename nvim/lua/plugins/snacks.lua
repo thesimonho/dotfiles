215710d87ae0219740622dc0ddd172415145377c
Simon Ho <simonho.ubc@gmail.com>
2026-01-23 14:03:18 -0800

    feat(nvim): add scope binds

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -129,6 +129,44 @@ return {
     opts = {
       bigfile = { enabled = true },
       quickfile = { enabled = true },
+      scope = {
+        enabled = true,
+        keys = {
+          textobject = {
+            is = {
+              min_size = 2, -- minimum size of the scope
+              edge = false, -- inner scope
+              cursor = false,
+              treesitter = { blocks = { enabled = false } },
+              desc = "inner scope",
+            },
+            as = {
+              cursor = false,
+              min_size = 2, -- minimum size of the scope
+              treesitter = { blocks = { enabled = false } },
+              desc = "full scope",
+            },
+          },
+          jump = {
+            ["[s"] = {
+              min_size = 1, -- allow single line scopes
+              bottom = false,
+              cursor = false,
+              edge = true,
+              treesitter = { blocks = { enabled = false } },
+              desc = "top edge of scope",
+            },
+            ["]s"] = {
+              min_size = 1, -- allow single line scopes
+              bottom = true,
+              cursor = false,
+              edge = true,
+              treesitter = { blocks = { enabled = false } },
+              desc = "bottom edge of scope",
+            },
+          },
+        },
+      },
       picker = {
         enabled = true,
         exclude = {
