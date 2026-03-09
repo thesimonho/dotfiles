065608c8a2c4292963647a3a742e63238e7a1816
Simon Ho <simonho.ubc@gmail.com>
2026-03-08 18:28:52 -0700

    fix(nix): set 600 permissions on ssh config

--- nix/modules/ssh.nix
+++ nix/modules/ssh.nix
@@ -134,4 +134,20 @@ in
     identityPaths = [ "${sshDir}/ssh_identity" ];
     secrets = sshSecrets // otherSecrets;
   };
+
+  # https://github.com/nix-community/home-manager/issues/322
+  home.file = {
+    # home-manager wrongly thinks it doesn't manage (and thus shouldn't clobber) this file due to the activation script
+    ".ssh/config".force = true;
+  };
+  home.activation = {
+    fixSshPermissions = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+      run install -d -m 0700 "$HOME/.ssh"
+      if [ -L "$HOME/.ssh/config" ]; then
+        src="$(readlink -f "$HOME/.ssh/config")"
+        run rm -f "$HOME/.ssh/config"
+        run install -m 0600 "$src" "$HOME/.ssh/config"
+      fi
+    '';
+  };
 }
