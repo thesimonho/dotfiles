a91b36e54833caf5f22ce918c472ac42344358c3
Simon Ho <simonho.ubc@gmail.com>
2026-05-16 03:22:37 -0700

    chore: flatten AI hook entrypoints

--- AI/hooks/post-edit-reminder.js
+++ AI/hooks/post-edit-reminder.js
@@ -3,7 +3,7 @@
  * Hook: Remind agents to verify after editing files.
  */
 
-const { addContext } = require("../../lib/hooks/hook-response");
+const { addContext } = require("../lib/hooks/hook-response");
 
 addContext(
   "PostToolUse",
